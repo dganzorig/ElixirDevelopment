9647e0be3663c29eccf7d3adda0d0b64efe59f7e
Dona Ganzorig <ganzorig0518@uchicago.edu>
2019-11-04 19:20:43 -0600

    Added authentication logic

---
+++ discuss/web/controllers/plugs/require_auth.ex
@@ -0,0 +1,21 @@
+defmodule Discuss.Plugs.RequireAuth do
+  import Plug.Conn
+  import Phoenix.Controller
+
+  alias Discuss.Router.Helpers
+
+  def init(_params) do
+  end
+
+  # params is whatever is returned from init function
+  def call(conn, _params) do
+    if conn.assigns[:user] do
+      conn # freely pass through plug
+    else
+      conn
+      |> put_flash(:error, "You must be logged in.")
+      |> redirect(to: Helpers.topic_path(conn, :index))
+      |> halt()
+    end
+  end
+end
